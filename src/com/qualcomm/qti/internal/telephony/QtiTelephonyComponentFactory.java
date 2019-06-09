f7291b12be06efee87e5e2a916423a4b2479dbfc
jhenrique09 <jhenrique09@users.noreply.github.com>
2019-06-09 11:40:11 +0000

    Rebrand Lineage -> Pearl

--- src/com/qualcomm/qti/internal/telephony/QtiTelephonyComponentFactory.java
+++ src/com/qualcomm/qti/internal/telephony/QtiTelephonyComponentFactory.java
@@ -23,18 +23,18 @@ import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.TelephonyComponentFactory;
 
-import com.qualcomm.qti.internal.telephony.LineageExtTelephony;
+import com.qualcomm.qti.internal.telephony.PearlExtTelephony;
 
 public class QtiTelephonyComponentFactory extends TelephonyComponentFactory {
 
     @Override
     public void makeExtTelephonyClasses(Context context,
             Phone[] phones, CommandsInterface[] commandsInterfaces) {
-        Rlog.d(LOG_TAG, "makeLineageExtTelephonyClasses");
+        Rlog.d(LOG_TAG, "makePearlExtTelephonyClasses");
         try {
-            LineageExtTelephony.init(context, phones, commandsInterfaces);
+            PearlExtTelephony.init(context, phones, commandsInterfaces);
         } catch (NoClassDefFoundError e) {
-            Rlog.e(LOG_TAG, "Error creating LineageExtTelephony", e);
+            Rlog.e(LOG_TAG, "Error creating PearlExtTelephony", e);
         }
     }
 }
